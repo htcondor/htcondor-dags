282dca9fad54602da248564a3ea5ae5e326e4f2d
JoshKarpel <karpel@wisc.edu>
2019-08-28 11:17:18 -0500

    correct master doc name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,8 @@ release = dags.__version__
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = "index"
+
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
